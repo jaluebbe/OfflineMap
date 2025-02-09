31df457502505bae68b094d6ed48872d08d163b4
Jannis Lübbe <luebbe@opensky-network.org>
2025-02-09 19:17:11 +0100

    moved some code to a separate router for handling the offline map data requests.

---
+++ offline_map/backend.py
@@ -0,0 +1,22 @@
+#!venv/bin/python
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+import routers.offline_map as offline_map
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/fonts", StaticFiles(directory="fonts"), name="fonts")
+
+app.include_router(offline_map.router)
+
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse("/static/leaflet_map.html")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
