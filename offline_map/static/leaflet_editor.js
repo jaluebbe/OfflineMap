16b94154c6e90a6213ba7bc7a7b4ec8af7a37cfc
Jannis Lübbe <jannis@jl82.de>
2025-03-12 07:50:50 +0100

    moved editor related code to separate file.

---
+++ offline_map/static/leaflet_editor.js
@@ -0,0 +1,52 @@
+map.createPane('editor');
+map.getPane('editor').style.zIndex = 392;
+
+let editorLayer = L.geoJSON([], {
+    pane: 'editor',
+}).addTo(map);
+layerControl.addOverlay(editorLayer, "Editor");
+map.pm.setGlobalOptions({
+    layerGroup: editorLayer,
+});
+
+function toggleMapClick(e) {
+    if (e.enabled) {
+        map.off('click', convertPosition);
+    } else {
+        map.on('click', convertPosition);
+    }
+}
+const pmEvents = [
+    'pm:globaleditmodetoggled',
+    'pm:globaldrawmodetoggled',
+    'pm:globalrotatemodetoggled',
+    'pm:globalcutmodetoggled',
+    'pm:globaldragmodetoggled',
+    'pm:globalremovalmodetoggled'
+];
+pmEvents.forEach(event => map.on(event, toggleMapClick));
+map.pm.addControls();
+
+L.Polygon.prototype.options.measurementOptions = {
+    ha: true,
+    showOnHover: true
+};
+L.Polyline.prototype.options.measurementOptions = {
+    showOnHover: true
+};
+L.Polyline.prototype.options.showMeasurements = true;
+
+const showMeasurements = L.layerGroup();
+layerControl.addOverlay(showMeasurements, "Show measurements");
+map.on('overlayadd', function(eventLayer) {
+    if (eventLayer.name === 'Show measurements') {
+        L.Polygon.prototype.options.measurementOptions.showOnHover = false;
+        L.Polyline.prototype.options.measurementOptions.showOnHover = false;
+    }
+});
+map.on('overlayremove', function(eventLayer) {
+    if (eventLayer.name === 'Show measurements') {
+        L.Polygon.prototype.options.measurementOptions.showOnHover = true;
+        L.Polyline.prototype.options.measurementOptions.showOnHover = true;
+    }
+});
