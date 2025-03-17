2738fdca38ac0c2ca9de32d4f4849e3e3b667071
Jannis Lübbe <jannisluebbe1982@gmail.com>
2025-03-17 00:55:02 +0000

    improved polygon cut behaviour.

--- offline_map/static/leaflet_editor.js
+++ offline_map/static/leaflet_editor.js
@@ -3,6 +3,29 @@ map.getPane('editor').style.zIndex = 392;
 
 const editorLayer = L.geoJSON([], {
     pane: 'editor',
+    onEachFeature: function(feature, layer) {
+        const properties = feature.properties || {};
+        layer.options.pmIgnore = !!properties.pmIgnore;
+        if (typeof properties.showMeasurements !== 'undefined') {
+            layer.options.showMeasurements = properties.showMeasurements;
+            if (typeof layer.showMeasurements === 'function' && layer.options.showMeasurements) {
+                layer.showMeasurements();
+            } else if (typeof layer.hideMeasurements === 'function') {
+                layer.hideMeasurements();
+            }
+        }
+    },
+    style: function(feature) {
+        const properties = feature.properties || {};
+        const style = {};
+        if (properties.color) {
+            style.color = properties.color;
+        }
+        if (properties.fill) {
+            style.fill = properties.fill;
+        }
+        return style;
+    }
 }).addTo(map);
 layerControl.addOverlay(editorLayer, "Editor");
 map.pm.setGlobalOptions({
@@ -21,28 +44,56 @@ const pmEvents = [
     'pm:globalremovalmodetoggled'
 ];
 pmEvents.forEach(event => map.on(event, toggleMapClick));
-map.pm.addControls();
+map.pm.addControls({
+    oneBlock: true,
+});
+
+map.on('pm:create', function(eo) {
+    const layer = eo.layer;
+    layer.feature = layer.feature || {
+        type: 'Feature',
+        properties: {}
+    };
+    if (typeof layer.showMeasurements === 'function') {
+        if (typeof layer.feature.properties.showMeasurements === 'undefined') {
+            layer.feature.properties.showMeasurements = true;
+        }
+        if (layer.feature.properties.showMeasurements) {
+            layer.showMeasurements();
+        } else {
+            layer.hideMeasurements();
+        }
+    }
+});
+
+map.on('pm:cut', function(eo) {
+    const originalLayer = eo.originalLayer;
+    const newLayer = eo.layer;
+    if (originalLayer?.feature) {
+        newLayer.feature = newLayer.feature || {
+            type: 'Feature',
+            properties: {}
+        };
+        newLayer.feature.properties = { ...originalLayer.feature.properties };
+        const tooltipContent = originalLayer.getTooltip()?.getContent();
+        const popupContent = originalLayer.getPopup()?.getContent();
+        if (tooltipContent) {
+            newLayer.bindTooltip(tooltipContent);
+        }
+        if (popupContent) {
+            newLayer.bindPopup(popupContent);
+        }
+        if (newLayer.feature.geometry?.type === 'MultiPolygon') {
+            const flattened = turf.flatten(newLayer.feature);
+            flattened.features.forEach((polygonFeature) => {
+                editorLayer.addData(polygonFeature)
+            });
+            editorLayer.removeLayer(newLayer);
+        }
+    }
+});
 
 L.Polygon.prototype.options.measurementOptions = {
     ha: true,
-    showOnHover: true
-};
-L.Polyline.prototype.options.measurementOptions = {
-    showOnHover: true
 };
 L.Polyline.prototype.options.showMeasurements = true;
-
-const showMeasurements = L.layerGroup();
-layerControl.addOverlay(showMeasurements, "Show measurements");
-map.on('overlayadd', function(eventLayer) {
-    if (eventLayer.name === 'Show measurements') {
-        L.Polygon.prototype.options.measurementOptions.showOnHover = false;
-        L.Polyline.prototype.options.measurementOptions.showOnHover = false;
-    }
-});
-map.on('overlayremove', function(eventLayer) {
-    if (eventLayer.name === 'Show measurements') {
-        L.Polygon.prototype.options.measurementOptions.showOnHover = true;
-        L.Polyline.prototype.options.measurementOptions.showOnHover = true;
-    }
-});
