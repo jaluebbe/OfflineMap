9704e2348a8b2d8418aaaa2911bac96b3fec7041
Jannis Lübbe <jannis@jl82.de>
2025-03-30 11:25:17 +0200

    centralised image path for leaflet-color-markers.

--- offline_map/static/leaflet/leaflet-color-markers.js
+++ offline_map/static/leaflet/leaflet-color-markers.js
@@ -1,46 +1,50 @@
 // Icons are from https://github.com/pointhi/leaflet-color-markers
+
+const MARKER_IMAGE_PATH = '/static/leaflet/images/';
+
 let ColorIcon = L.Icon.extend({
     options: {
-        shadowUrl: 'leaflet/images/marker-shadow.png',
+        shadowUrl: `${MARKER_IMAGE_PATH}marker-shadow.png`,
         iconSize: [25, 41],
         shadowSize: [41, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34]
     }
 });
+
 const blueIcon = new ColorIcon({
-    iconUrl: 'leaflet/images/marker-icon-blue.png',
-    iconRetinaUrl: 'leaflet/images/marker-icon-2x-blue.png'
+    iconUrl: `${MARKER_IMAGE_PATH}marker-icon-blue.png`,
+    iconRetinaUrl: `${MARKER_IMAGE_PATH}marker-icon-2x-blue.png`
 });
 const goldIcon = new ColorIcon({
-    iconUrl: 'leaflet/images/marker-icon-gold.png',
-    iconRetinaUrl: 'leaflet/images/marker-icon-2x-gold.png'
+    iconUrl: `${MARKER_IMAGE_PATH}marker-icon-gold.png`,
+    iconRetinaUrl: `${MARKER_IMAGE_PATH}marker-icon-2x-gold.png`
 });
 const redIcon = new ColorIcon({
-    iconUrl: 'leaflet/images/marker-icon-red.png',
-    iconRetinaUrl: 'leaflet/images/marker-icon-2x-red.png'
+    iconUrl: `${MARKER_IMAGE_PATH}marker-icon-red.png`,
+    iconRetinaUrl: `${MARKER_IMAGE_PATH}marker-icon-2x-red.png`
 });
 const greenIcon = new ColorIcon({
-    iconUrl: 'leaflet/images/marker-icon-green.png',
-    iconRetinaUrl: 'leaflet/images/marker-icon-2x-green.png'
+    iconUrl: `${MARKER_IMAGE_PATH}marker-icon-green.png`,
+    iconRetinaUrl: `${MARKER_IMAGE_PATH}marker-icon-2x-green.png`
 });
 const orangeIcon = new ColorIcon({
-    iconUrl: 'leaflet/images/marker-icon-orange.png',
-    iconRetinaUrl: 'leaflet/images/marker-icon-2x-orange.png'
+    iconUrl: `${MARKER_IMAGE_PATH}marker-icon-orange.png`,
+    iconRetinaUrl: `${MARKER_IMAGE_PATH}marker-icon-2x-orange.png`
 });
 const yellowIcon = new ColorIcon({
-    iconUrl: 'leaflet/images/marker-icon-yellow.png',
-    iconRetinaUrl: 'leaflet/images/marker-icon-2x-yellow.png'
+    iconUrl: `${MARKER_IMAGE_PATH}marker-icon-yellow.png`,
+    iconRetinaUrl: `${MARKER_IMAGE_PATH}marker-icon-2x-yellow.png`
 });
 const violetIcon = new ColorIcon({
-    iconUrl: 'leaflet/images/marker-icon-violet.png',
-    iconRetinaUrl: 'leaflet/images/marker-icon-2x-violet.png'
+    iconUrl: `${MARKER_IMAGE_PATH}marker-icon-violet.png`,
+    iconRetinaUrl: `${MARKER_IMAGE_PATH}marker-icon-2x-violet.png`
 });
 const greyIcon = new ColorIcon({
-    iconUrl: 'leaflet/images/marker-icon-grey.png',
-    iconRetinaUrl: 'leaflet/images/marker-icon-2x-grey.png'
+    iconUrl: `${MARKER_IMAGE_PATH}marker-icon-grey.png`,
+    iconRetinaUrl: `${MARKER_IMAGE_PATH}marker-icon-2x-grey.png`
 });
 const blackIcon = new ColorIcon({
-    iconUrl: 'leaflet/images/marker-icon-black.png',
-    iconRetinaUrl: 'leaflet/images/marker-icon-2x-black.png'
+    iconUrl: `${MARKER_IMAGE_PATH}marker-icon-black.png`,
+    iconRetinaUrl: `${MARKER_IMAGE_PATH}marker-icon-2x-black.png`
 });
