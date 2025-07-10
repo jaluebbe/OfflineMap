42b2ecf49cf97f62408f416fa90730ae98000dfc
Jannis Lübbe <jannis@jl82.de>
2025-07-11 00:27:41 +0200

    added first draft of EuroPython talk.

---
+++ offline_map/static/europython_talk/radio_replay.js
@@ -0,0 +1,69 @@
+const messages = [
+    "Provide coordinates for assembly point in the Stadtwerke parking lot, Luisenstraße, 49074 Osnabrück.",
+    "Evacuation required in a 1000 m radius around 32UMC36339139.",
+    "Handle operational area A southwest of the railway tracks.",
+    "Detmarstrasse is cleared.",
+    "Person at Kollegienwall 11 refuses evacuation. Police required.",
+    "Schlagvorder Strasse is cleared.",
+    "Police arrived at Kollegienwall 11 and removed the person. Kollegienwall is cleared.",
+];
+
+function createPopupManager() {
+    let currentMessageIndex = 0;
+
+    const popup = document.createElement('div');
+    popup.id = 'popup';
+    popup.className = 'popup';
+    popup.style.display = 'none';
+    popup.innerHTML = `
+    <span id="popupClose" class="popup-close">&times;</span>
+    <div id="popupMessage"></div>
+    `;
+    document.body.appendChild(popup);
+
+    function closePopup() {
+        currentMessageIndex++;
+        if (currentMessageIndex < messages.length) {
+            updatePopup(messages[currentMessageIndex]);
+        } else {
+            popup.style.display = 'none';
+        }
+    }
+
+    document.getElementById('popupClose').onclick = closePopup;
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {
+            closePopup();
+        }
+    });
+
+    function updatePopup(message) {
+        document.getElementById('popupMessage').textContent = message;
+        popup.style.display = 'block';
+    }
+
+    return {
+        showFirstMessage: () => updatePopup(messages[currentMessageIndex])
+    };
+}
+
+function createRadioButton(popupManager) {
+    const button = document.createElement('button');
+    button.id = 'radioButton';
+    button.className = 'radio-button';
+    button.innerHTML = '<img src="../pycon_talk/img/walkie_talkie.svg" alt="Radio" class="radio-icon">';
+    document.body.appendChild(button);
+    button.onclick = function() {
+        popupManager.showFirstMessage();
+        button.remove();
+    };
+}
+
+function initialize() {
+    const popupManager = createPopupManager();
+    createRadioButton(popupManager);
+}
+
+
+initialize();
