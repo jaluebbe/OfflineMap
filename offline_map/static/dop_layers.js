db2f4c3697a359b12e708d9603aa3f3a3da1c426
Jannis Lübbe <jannisluebbe1982@gmail.com>
2026-08-05 21:28:34 +0000

    fixed DOP layers, especially for Hamburg.

--- offline_map/static/dop_layers.js
+++ offline_map/static/dop_layers.js
@@ -1,22 +1,26 @@
-var wmsHamburgDOP = L.tileLayer.wms('https://geodienste.hamburg.de/HH_WMS_DOP', {
-    layers: 'DOP',
+var wmsHamburgDOP = L.tileLayer.wms('https://geodienste.hamburg.de/wms_dop_zeitreihe_belaubt', {
+    layers: 'dop_zeitreihe_belaubt',
     format: 'image/png',
     transparent: true,
-    attribution: '&copy <a href="https://www.hamburg.de/bsw/landesbetrieb-geoinformation-und-vermessung/">' +
-        'Freie und Hansestadt Hamburg, LGV</a>',
+    version: '1.3.0',
+    attribution: '&copy; <a href="https://www.hamburg.de/bsw/landesbetrieb-geoinformation-und-vermessung/">' +
+        'Freie und Hansestadt Hamburg, LGV</a> ' +
+        '(<a href="https://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a>)',
     minZoom: 12,
+    maxNativeZoom: 19,
     maxZoom: 22,
     bounds: [
-        [53.3, 8.4],
-        [54, 10.4]
+        [53.38, 9.72],
+        [53.75, 10.35]
     ]
 });
 var wmsNiDOP = L.tileLayer.wms('https://opendata.lgln.niedersachsen.de/doorman/noauth/dop_wms', {
     layers: 'ni_dop20',
     format: 'image/png',
     transparent: true,
-    attribution: '&copy <a href="https://www.lgln.niedersachsen.de">LGLN</a> (2025) <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>',
+    attribution: '&copy; <a href="https://www.lgln.niedersachsen.de">LGLN</a> (2025) <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>',
     minZoom: 12,
+    maxNativeZoom: 19,
     maxZoom: 22,
     bounds: [
         [51.29, 6.6],
@@ -27,8 +31,9 @@ var wmsNWDOP = L.tileLayer.wms('https://www.wms.nrw.de/geobasis/wms_nw_dop', {
     layers: 'nw_dop_rgb',
     format: 'image/png',
     transparent: true,
-    attribution: '&copy <a href="https://www.bezreg-koeln.nrw.de/geobasis-nrw">Bezirksregierung Köln</a>',
+    attribution: '&copy; <a href="https://www.bezreg-koeln.nrw.de/geobasis-nrw">Bezirksregierung Köln</a>',
     minZoom: 12,
+    maxNativeZoom: 19,
     maxZoom: 22,
     bounds: [
         [50.3, 5.8],
@@ -39,8 +44,9 @@ var wmsSHDOP = L.tileLayer.wms('https://dienste.gdi-sh.de/WMS_SH_DOP20col_OpenGB
     layers: 'sh_dop20_rgb',
     format: 'image/png',
     transparent: true,
-    attribution: '&copy GeoBasis-DE/LVermGeo SH/<a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>',
+    attribution: '&copy; GeoBasis-DE/LVermGeo SH/<a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>',
     minZoom: 12,
+    maxNativeZoom: 19,
     maxZoom: 22,
     bounds: [
         [55, 7.8],
@@ -51,8 +57,9 @@ var wmsHBDOP = L.tileLayer.wms('https://geodienste.bremen.de/wms_dop20_2023', {
     layers: 'DOP20_2023_HB,DOP20_2023_BHV',
     format: 'image/png',
     transparent: true,
-    attribution: '&copy <a href="https://www.geo.bremen.de/">Landesamt GeoInformation Bremen</a>',
+    attribution: '&copy; <a href="https://www.geo.bremen.de/">Landesamt GeoInformation Bremen</a>',
     minZoom: 12,
+    maxNativeZoom: 19,
     maxZoom: 22,
     bounds: [
         [53, 8.4],
@@ -63,8 +70,9 @@ var wmsHeDOP = L.tileLayer.wms('https://www.gds-srv.hessen.de/cgi-bin/lika-servi
     layers: 'he_dop_rgb',
     format: 'image/png',
     transparent: true,
-    attribution: '&copy <a href="https://hvbg.hessen.de/">HVBG</a>',
+    attribution: '&copy; <a href="https://hvbg.hessen.de/">HVBG</a>',
     minZoom: 12,
+    maxNativeZoom: 19,
     maxZoom: 22,
     bounds: [
         [49.25, 7.41867],
@@ -75,8 +83,9 @@ var wmsSLDOP = L.tileLayer.wms('https://geoportal.saarland.de/freewms/dop2023', 
     layers: 'sl_dop20_rgb',
     format: 'image/png',
     transparent: true,
-    attribution: '&copy GeoBasis DE/LVGL-SL (2025) <a href="https://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a>',
+    attribution: '&copy; GeoBasis DE/LVGL-SL (2025) <a href="https://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a>',
     minZoom: 12,
+    maxNativeZoom: 19,
     maxZoom: 22,
     bounds: [
         [49.0829, 6.3406],
@@ -87,7 +96,7 @@ var wmsRPDOP = L.tileLayer.wms('https://geo4.service24.rlp.de/wms/rp_dop20.fcgi'
     layers: 'rp_dop20',
     format: 'image/png',
     transparent: true,
-    attribution: '&copy GeoBasis-DE / <a href="http://www.lvermgeo.rlp.de">LVermGeoRP</a> (2025) <a href="https://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a> [Daten bearbeitet]',
+    attribution: '&copy; GeoBasis-DE / <a href="http://www.lvermgeo.rlp.de">LVermGeoRP</a> (2025) <a href="https://www.govdata.de/dl-de/by-2-0">dl-de/by-2-0</a> [Daten bearbeitet]',
     minZoom: 12,
     maxZoom: 22,
     bounds: [
